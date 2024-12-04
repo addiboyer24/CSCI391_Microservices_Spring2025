26e24fb0ce90758b35b022cbf3475c60d9400eeb
Addison Boyer <AddisonB@consumerdirectcare.com>
2024-12-04 09:53:42 -0700

    removing GitHub workflow

--- src/test/java/demo/restfulservice/RestfulServiceApplicationTests.java
+++ src/test/java/demo/restfulservice/RestfulServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package demo.restfulservice;
 
+import App.Startup;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Startup.class)
 class RestfulServiceApplicationTests {
 
 	@Test
